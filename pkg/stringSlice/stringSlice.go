931448cb5e813f32aaf23b13fb9ef84f36dbaa95
ariary <ariary9.2@hotmail.fr>
2022-03-02 06:16:17 -0500

    cartesian product

---
+++ pkg/stringSlice/stringSlice.go
@@ -0,0 +1,31 @@
+package stringslice
+
+//cartesianProductPlusPlus: Perform cartesian product between a slice of string slice and a string slice. Beware: complexity -> quadratic
+func cartesianProduct(list1 [][]string, list2 []string) (product [][]string) {
+	var lenProduct int
+	if len(list1) == 1 {
+		lenProduct = len(list1) * (len(list2) - 1)
+	} else {
+		lenProduct = len(list1) * (len(list2))
+	}
+	product = make([][]string, lenProduct)
+	productIndex := 0
+	for i := 0; i < len(list1); i++ { //for each item of first list
+		for j := 0; j < len(list2); j++ { //couple it with other
+			product[productIndex] = append(product[productIndex], list1[i]...)
+			product[productIndex] = append(product[productIndex], list2[j])
+			productIndex++
+		}
+	}
+	return product
+}
+
+//Return true if  specific string is within a string slice
+func Contains(s []string, e string) bool {
+	for _, a := range s {
+		if a == e {
+			return true
+		}
+	}
+	return false
+}
