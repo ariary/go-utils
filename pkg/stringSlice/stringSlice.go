7d78e96d63f2b6b10f7bffd22b1e43e51130181c
ariary <ariary9.2@hotmail.fr>
2022-05-13 11:49:42 -0400

    add chunk string

--- pkg/stringSlice/stringSlice.go
+++ pkg/stringSlice/stringSlice.go
@@ -29,3 +29,26 @@ func Contains(s []string, e string) bool {
 	}
 	return false
 }
+
+//ChunksString: split a string in chunk of fixed size
+func ChunksString(s string, chunkSize int) []string {
+	if len(s) == 0 {
+		return nil
+	}
+	if chunkSize >= len(s) {
+		return []string{s}
+	}
+	var chunks []string = make([]string, 0, (len(s)-1)/chunkSize+1)
+	currentLen := 0
+	currentStart := 0
+	for i := range s {
+		if currentLen == chunkSize {
+			chunks = append(chunks, s[currentStart:i])
+			currentLen = 0
+			currentStart = i
+		}
+		currentLen++
+	}
+	chunks = append(chunks, s[currentStart:])
+	return chunks
+}
